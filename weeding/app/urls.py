b61bd0e7fc86b947f96e03fbea353a9853ed2bac
Jovialngandu <jovialngandu2@gmail.com>
2024-07-14 22:04:04 +0200

    [ADD] Mise en place du template de gestion des demandes

--- weeding/app/urls.py
+++ weeding/app/urls.py
@@ -4,6 +4,7 @@ from app.view import inscription
 from app.view import weeding
 from app.view import profil
 from app.view import couples
+from app.view import demandes
 from . import views
 
 urlpatterns = [
@@ -12,6 +13,7 @@ urlpatterns = [
     path("signin",inscription.inscription, name="inscription"),
     path("weeding",weeding.mariage, name="mariage"),
     path("profil",profil.profil, name="profil"),
-    path("couples",couples.couples, name="couples")
+    path("couples",couples.couples, name="couples"),
+    path("demandes",demandes.demandes, name="demandes"),
     
 ]
