6af8074a684dcf2ea88d13d9f4891793a736a0b0
Jovialngandu <jovialngandu2@gmail.com>
2024-07-17 03:43:39 +0200

    [UPDATE] Test sur le modele
    si tout va bien on doit avoir un  {'nbr': 0} ou {'nbr': un nombre quelconque}

--- weeding/app/models/Couple.py
+++ weeding/app/models/Couple.py
@@ -2,5 +2,5 @@ from django.db import models
 from app.models import Person
 
 class Couple(models.Model):
-    person1 = models.ForeignKey(Person, on_delete=models.CASCADE)
-    person2 = models.ForeignKey(Person, on_delete=models.CASCADE)
+    person1 = models.ForeignKey(Person, on_delete=models.CASCADE,related_name='person1')
+    person2 = models.ForeignKey(Person, on_delete=models.CASCADE,related_name='person2')
