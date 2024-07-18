5e9c79de09af1d5c2317716c3bc1847398d9160e
gedeontimothy <gedeonbateko3@gmail.com>
2024-07-18 03:01:25 +0100

    [UPDATE] Bug | Model - Correction des bugs liés à une erreur d'appel de `from app.models.Person import Person` dans le modèle `PhysicalAbility` et correction du bug d'indentation du modèle `Request`

--- weeding/app/models/Request.py
+++ weeding/app/models/Request.py
@@ -4,9 +4,9 @@ from app.models.Couple import Couple
 class Request(models.Model):
     request_date = models.DateField()
     request_status = models.CharField(max_length = 1, choices=[
-		('0', 'En attente'),
-		('1', 'Valide'),
-		('2', 'Non valide')
-	])
+        ('0', 'En attente'),
+        ('1', 'Valide'),
+        ('2', 'Non valide')
+    ])
     rejection_reason = models.TextField(null=True, blank=True)
     couple = models.ForeignKey(Couple, on_delete=models.CASCADE)
