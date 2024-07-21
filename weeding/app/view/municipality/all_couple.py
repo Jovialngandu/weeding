4974d891981a6fc69c92d5137727fc0ddec945b9
Jovialngandu <jovialngandu2@gmail.com>
2024-07-21 07:43:22 +0200

    [UPDATE] Recuperation des données necessaire à la view des couples

--- weeding/app/view/municipality/all_couple.py
+++ weeding/app/view/municipality/all_couple.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from app.models.Couple import  Couple
 from django.http import HttpResponse
+from app.models  import Marriage
+from app.models  import Person
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+@method_decorator(login_required(login_url='/login'), name='dispatch') # sur `dispatch` : pour toutes les méthode HTTP(post, get, ...)
 
 class couples(TemplateView):
     template_name = "app/municipality/all_couple.html"
    
     def get_queryset(self):
-            return Couple.objects.all()
+           return ''
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["couples"] = self.get_queryset()
-        # print(context)
+        #RECUPERATION DES INFO DU USER CONNECTER
+        context['person_id']=self.request.user.person_id
+        user_connected=Person.objects.get(pk=context['person_id'])    
+        context['profil']=user_connected 
+        context['user']=self.request.user
+        context["marriage"] = Marriage.objects.all()
+        
+        
         return context
